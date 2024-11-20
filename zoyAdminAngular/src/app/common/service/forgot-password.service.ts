326303b782bf3026fb735c7fdd8d1f8408b7273d
anand-dbq <anand.m@mydbq.com>
2024-11-20 13:16:48 +0530

    removed-codeService

--- zoyAdminAngular/src/app/common/service/forgot-password.service.ts
+++ zoyAdminAngular/src/app/common/service/forgot-password.service.ts
@@ -17,23 +17,7 @@ import { MessageService } from 'src/app/message.service';
     constructor(private httpclient:HttpClient,@Inject(BASE_PATH) private basePath:string, private messageService:MessageService){
 
     }
-    // public find(empCode:string,pageIndex:number,pageSize:number): Observable<any> {
-    //     const url1=this.basePath +'userrole/user/showListOfUsers';
-    //     let param={"empCode":empCode,"pageIndex":pageIndex,"pageSize":pageSize};
-    //           return this.httpclient.post<any[]>(
-    //               url1,
-    //               param
-    //               ,
-    //               {
-    //                   headers:ServiceHelper.buildHeaders(),
-    //                  observe : 'body',
-    //                  withCredentials:true
-    //               }
-    //           );
-    //   } 
-
-
-      public rolesDropdown(): Observable<any> {
+       public rolesDropdown(): Observable<any> {
         const url1=this.basePath +'userrole/user/showRolesDropdown';
         return this.httpclient.post<String[]>(
             url1,
