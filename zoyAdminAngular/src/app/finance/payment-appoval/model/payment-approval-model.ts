e2cce71a49ba1a2aa7f8cd10d16cb9b51586de80
anand-dbq <anand.m@mydbq.com>
2024-11-23 00:04:35 +0530

    payment approval changes

--- zoyAdminAngular/src/app/finance/payment-appoval/model/payment-approval-model.ts
+++ zoyAdminAngular/src/app/finance/payment-appoval/model/payment-approval-model.ts
@@ -4,5 +4,5 @@ export class PaymentApprovalModel{
   total_amount: string;
   transaction_date: string;
   transaction_no: string;
-  transaction_approval: string;	
+  transaction_approval: 'Received' |' Not Received';	
 }
