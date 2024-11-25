9336c93e34dc939a7ea0d351964e6204b0efa286
mahen <mahendrabehera@mydbq.com>
2024-11-25 14:44:13 +0530

    external server url added in env.ts file

--- zoyAdminAngular/src/app/common/shared/variables.ts
+++ zoyAdminAngular/src/app/common/shared/variables.ts
@@ -2,5 +2,6 @@ import { InjectionToken } from '@angular/core';
 import { DefMenu } from './def-menu';
 
 export const BASE_PATH =new InjectionToken<string>('basePath');
+export const BASE_PATH_EXTERNAL_SERVER =new InjectionToken<string>('basePathExternal');
 
 export const defMenuEnable =new InjectionToken<DefMenu>('defMenuEnable');
