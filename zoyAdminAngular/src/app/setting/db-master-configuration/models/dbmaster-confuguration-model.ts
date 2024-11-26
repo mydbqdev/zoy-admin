82cc5185e02c42cf7798f9fe10d8d1760cba620c
anand-dbq <anand.m@mydbq.com>
2024-11-26 15:55:32 +0530

    db master configuration component

---
+++ zoyAdminAngular/src/app/setting/db-master-configuration/models/dbmaster-confuguration-model.ts
@@ -0,0 +1,6 @@
+export class DbMasterConfiguration {
+      firstName:string;
+      lastName: string;
+      userEmail: string;
+      designation:string;
+  }
--- zoyAdminAngular/src/app/setting/settings-menu-routing.module.ts
+++ zoyAdminAngular/src/app/setting/settings-menu-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RoleMasterComponent } from './role-master/components/role-master.component';
+import { DbMasterConfigurationComponent } from './db-master-configuration/components/db-master-configuration.component';
 
 const routes: Routes = [
   { path: 'role-master', component: RoleMasterComponent},
+  { path: 'db-master-configuration', component: DbMasterConfigurationComponent},
 ];
 
 @NgModule({
