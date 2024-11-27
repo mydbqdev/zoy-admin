c09a37c064dffc5d4842d6ad9d70c155d474f62e
mahen <mahendrabehera@mydbq.com>
2024-11-27 16:57:29 +0530

    home page bar chart

--- zoyAdminAngular/src/app/chart/barchart-ngx.ts
+++ zoyAdminAngular/src/app/chart/barchart-ngx.ts
@@ -2,27 +2,35 @@ import { Component } from '@angular/core';
 import { single } from './chart-data';
 @Component({
   selector: 'barchart-ngx',
-  template: `<ngx-charts-bar-vertical [view]="[700, 400]" [results]="single" [gradient]="gradient" [xAxis]="showXAxis"
+  template: `<ngx-charts-bar-vertical [view]="[420, 400]" [showGridLines]="showGridLines" [customColors]="colorScheme" [results]="single" [gradient]="gradient" [xAxis]="showXAxis"
   [yAxis]="showYAxis" [legend]="showLegend" [showXAxisLabel]="showXAxisLabel" [showYAxisLabel]="showYAxisLabel" [xAxisLabel]="xAxisLabel"
   [yAxisLabel]="yAxisLabel" (select)="onSelect($event)">
  </ngx-charts-bar-vertical>`
 })
 export class BarNgxChartComponent {
-  view: any[] = [700, 400];
+  view: any[] = [400, 300];
   single: any[];
   // options
+  showGridLines=false;
   showXAxis = true;
   showYAxis = true;
   gradient = false;
-  showLegend = true;
+  showLegend = false;
   showXAxisLabel = false;
   xAxisLabel = '';
-  showYAxisLabel = false;
-  yAxisLabel = '';
+  showYAxisLabel = true;
+  yAxisLabel = 'In Thousand';
 
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
-  };
+  colorScheme = [
+        {"name": "01/11","value":'#F989E7'}, 
+        {"name": "02/11","value":'#F989E7'},
+        {"name": "03/11","value":'#F989E7'},
+        {"name": "04/11","value":'#F989E7'}, 
+        {"name": "05/11","value":'#F989E7'},
+        {"name": "06/11","value":'#F989E7'},
+        {"name": "07/11","value":'#F989E7'}
+  ];
+
 
   constructor() {
     Object.assign(this, { single });
--- zoyAdminAngular/src/app/chart/chart-data.ts
+++ zoyAdminAngular/src/app/chart/chart-data.ts
@@ -1,14 +1,75 @@
 export var single = [
     {
-      "name": "Germany",
+      "name": "01/11",
       "value": 25
     },
     {
-      "name": "USA",
+      "name": "02/11",
       "value": 50
     },
     {
-      "name": "France",
+      "name": "03/11",
       "value": 72
-    } 
+    }, 
+    {
+      "name": "04/11",
+      "value": 25
+    },
+    {
+      "name": "05/11",
+      "value": 50
+    },
+    {
+      "name": "06/11",
+      "value": 72
+    },
+    {
+      "name": "07/11",
+      "value": 72
+    }  
   ];
+
+  export var multi = [
+    {
+      "name": "Germany",
+      "series": [
+        {
+          "name": "2010",
+          "value": 7300000
+        },
+        {
+          "name": "2011",
+          "value": 8940000
+        }
+      ]
+    },
+  
+    {
+      "name": "USA",
+      "series": [
+        {
+          "name": "2010",
+          "value": 7870000
+        },
+        {
+          "name": "2011",
+          "value": 8270000
+        }
+      ]
+    },
+  
+    {
+      "name": "France",
+      "series": [
+        {
+          "name": "2010",
+          "value": 5000002
+        },
+        {
+          "name": "2011",
+          "value": 5800000
+        }
+      ]
+    }
+  ];
+  
