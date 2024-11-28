dd67b23fc2a099c2959c951393ffdbf6a6314957
mahen <mahendrabehera@mydbq.com>
2024-11-28 19:28:19 +0530

    bar chart

--- zoyAdminAngular/src/app/chart/chart-data.ts
+++ zoyAdminAngular/src/app/chart/chart-data.ts
@@ -29,6 +29,107 @@ export var single = [
     }  
   ];
 
+  export var halfYr = [
+    {
+      "name": "01/11",
+      "value": 25
+    },
+    {
+      "name": "02/11",
+      "value": 50
+    },
+    {
+      "name": "03/11",
+      "value": 72
+    }, 
+    {
+      "name": "04/11",
+      "value": 25
+    },
+    {
+      "name": "05/11",
+      "value": 50
+    },
+    {
+      "name": "06/11",
+      "value": 72
+    } 
+  ];
+
+  export var monthly = [
+    {
+      "name": "01/11",
+      "value": 25
+    },
+    {
+      "name": "02/11",
+      "value": 50
+    },
+    {
+      "name": "03/11",
+      "value": 72
+    }, 
+    {
+      "name": "04/11",
+      "value": 25
+    },
+    {
+      "name": "05/11",
+      "value": 50
+    }
+  ];
+
+  export var yearly = [
+    {
+      "name": "01/11",
+      "value": 25
+    },
+    {
+      "name": "02/11",
+      "value": 50
+    },
+    {
+      "name": "03/11",
+      "value": 72
+    }, 
+    {
+      "name": "04/11",
+      "value": 25
+    },
+    {
+      "name": "05/11",
+      "value": 50
+    },
+    {
+      "name": "06/11",
+      "value": 25
+    },
+    {
+      "name": "07/11",
+      "value": 50
+    },
+    {
+      "name": "08/11",
+      "value": 72
+    }, 
+    {
+      "name": "09/11",
+      "value": 25
+    },
+    {
+      "name": "10/11",
+      "value": 50
+    },
+    {
+      "name": "11/11",
+      "value": 25
+    },
+    {
+      "name": "12/11",
+      "value": 50
+    }
+  ];
+
   export var multi = [
     {
       "name": "Germany",
