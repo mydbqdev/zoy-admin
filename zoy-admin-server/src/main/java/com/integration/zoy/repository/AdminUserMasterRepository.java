ccb054bc76fcf28b169d882598595d2deeac7470
Pavan4517 <pavan.d@mydbq.com>
2024-12-18 10:12:26 +0530

    profile repo added

--- zoy-admin-server/src/main/java/com/integration/zoy/repository/AdminUserMasterRepository.java
+++ zoy-admin-server/src/main/java/com/integration/zoy/repository/AdminUserMasterRepository.java
@@ -15,40 +15,40 @@ import com.integration.zoy.entity.AdminUserMaster;
 public interface AdminUserMasterRepository extends JpaRepository<AdminUserMaster, String> {
 
 	@Query(value = "SELECT " +
-	        "um.first_name AS firstName, " +
-	        "um.last_name AS lastName, " +
-	        "um.user_email AS userEmail, " +
-	        "um.contact_number AS contactNumber, " +
-	        "um.designation AS designation, " +
-	        "um.status AS status, " +
-	        "ar.id AS roleId, " +
-	        "ar.role_name AS roleName, " +
-	        "CASE WHEN ut.is_approve = true THEN 'approved' ELSE 'pending' END AS approveStatus, " +
-	        "STRING_AGG(DISTINCT CASE " +
-	        "WHEN ut.is_approve = true THEN " +
-	        "CASE " +
-	        "WHEN rs.read_prv = true AND rs.write_prv = true THEN upper(rs.screen_name) || '_READ,' || upper(rs.screen_name) || '_WRITE' " +
-	        "WHEN rs.read_prv = true THEN upper(rs.screen_name) || '_READ' " +
-	        "WHEN rs.write_prv = true THEN upper(rs.screen_name) || '_WRITE' " +
-	        "ELSE upper(rs.screen_name) " +
-	        "END " +
-	        "ELSE NULL END, ',') AS approvedPrivilege, " +
-	        "STRING_AGG(DISTINCT CASE " +
-	        "WHEN ut.is_approve = false THEN " +
-	        "CASE " +
-	        "WHEN rs.read_prv = true AND rs.write_prv = true THEN upper(rs.screen_name) || '_READ,' || upper(rs.screen_name) || '_WRITE' " +
-	        "WHEN rs.read_prv = true THEN upper(rs.screen_name) || '_READ' " +
-	        "WHEN rs.write_prv = true THEN upper(rs.screen_name) || '_WRITE' " +
-	        "ELSE upper(rs.screen_name) " +
-	        "END " +
-	        "ELSE NULL END, ',') AS unapprovedPrivilege " +
-	        "FROM pgadmin.user_master um " +
-	        "LEFT JOIN pgadmin.user_temprory ut ON um.user_email = ut.user_email " +
-	        "LEFT JOIN pgadmin.user_role ur ON ur.user_email = um.user_email " +
-	        "LEFT JOIN pgadmin.app_role ar ON ur.role_id = ar.id " +
-	        "LEFT JOIN pgadmin.role_screen rs ON ar.id = rs.role_id " +
-	        "GROUP BY um.user_email, ar.id, ar.role_name, ut.is_approve", 
-	        nativeQuery = true)
+			"um.first_name AS firstName, " +
+			"um.last_name AS lastName, " +
+			"um.user_email AS userEmail, " +
+			"um.contact_number AS contactNumber, " +
+			"um.designation AS designation, " +
+			"um.status AS status, " +
+			"ar.id AS roleId, " +
+			"ar.role_name AS roleName, " +
+			"CASE WHEN ut.is_approve = true THEN 'approved' ELSE 'pending' END AS approveStatus, " +
+			"STRING_AGG(DISTINCT CASE " +
+			"WHEN ut.is_approve = true THEN " +
+			"CASE " +
+			"WHEN rs.read_prv = true AND rs.write_prv = true THEN upper(rs.screen_name) || '_READ,' || upper(rs.screen_name) || '_WRITE' " +
+			"WHEN rs.read_prv = true THEN upper(rs.screen_name) || '_READ' " +
+			"WHEN rs.write_prv = true THEN upper(rs.screen_name) || '_WRITE' " +
+			"ELSE upper(rs.screen_name) " +
+			"END " +
+			"ELSE NULL END, ',') AS approvedPrivilege, " +
+			"STRING_AGG(DISTINCT CASE " +
+			"WHEN ut.is_approve = false THEN " +
+			"CASE " +
+			"WHEN rs.read_prv = true AND rs.write_prv = true THEN upper(rs.screen_name) || '_READ,' || upper(rs.screen_name) || '_WRITE' " +
+			"WHEN rs.read_prv = true THEN upper(rs.screen_name) || '_READ' " +
+			"WHEN rs.write_prv = true THEN upper(rs.screen_name) || '_WRITE' " +
+			"ELSE upper(rs.screen_name) " +
+			"END " +
+			"ELSE NULL END, ',') AS unapprovedPrivilege " +
+			"FROM pgadmin.user_master um " +
+			"LEFT JOIN pgadmin.user_temprory ut ON um.user_email = ut.user_email " +
+			"LEFT JOIN pgadmin.user_role ur ON ur.user_email = um.user_email " +
+			"LEFT JOIN pgadmin.app_role ar ON ur.role_id = ar.id " +
+			"LEFT JOIN pgadmin.role_screen rs ON ar.id = rs.role_id " +
+			"GROUP BY um.user_email, ar.id, ar.role_name, ut.is_approve", 
+			nativeQuery = true)
 	List<Object[]> findAllAdminUserPrivilegesOld();
 
 	@Query(value = "select distinct * from ("
@@ -79,7 +79,7 @@ public interface AdminUserMasterRepository extends JpaRepository<AdminUserMaster
 			+ "	       GROUP BY um.user_email ,ut.role_id,ut.is_approve, art.role_name,ut.is_rejected)pre ", nativeQuery = true)	
 	List<Object[]> findAllAdminUserPrivileges();
 
-	
+
 	@Query(value="select um.first_name,um.last_name, "
 			+ "um.user_email, "
 			+ "um.contact_number, "
@@ -91,14 +91,14 @@ public interface AdminUserMasterRepository extends JpaRepository<AdminUserMaster
 			+ "as approved_privilege  from pgadmin.user_master um left join pgadmin.user_role ur on um.user_email =ur.user_email  "
 			+ "left join pgadmin.role_screen rs on rs.role_id =ur.role_id where um.user_email=:emailId group by um.user_email",nativeQuery = true)
 	List<String[]> findAllAdminUserDetails(String emailId);
-	
+
 	@Transactional
 	@Modifying
 	@Query(value = "INSERT INTO user_role (user_email, role_id) " +
-            "SELECT user_email, role_id FROM user_temprory " +
-            "WHERE is_approve = false AND is_rejected = false AND user_email = :user_email", nativeQuery = true)
-        void insertUserDetails(String user_email);
-	
+			"SELECT user_email, role_id FROM user_temprory " +
+			"WHERE is_approve = false AND is_rejected = false AND user_email = :user_email", nativeQuery = true)
+	void insertUserDetails(String user_email);
+
 	/** approve role time old record will be deleted from table and insert new record
 	 * 
 	 * */
@@ -106,23 +106,23 @@ public interface AdminUserMasterRepository extends JpaRepository<AdminUserMaster
 	@Modifying
 	@Query(value = "DELETE from user_role WHERE user_email = :user_email", nativeQuery = true)
 	void approveUser(String user_email);
-	
+
 	@Transactional
 	@Modifying
 	@Query(value = "DELETE from user_temprory WHERE user_email = :user_email", nativeQuery = true)
 	void rejectUser(String user_email);
-	
+
 	@Query(value="select ar.role_name from user_role ur JOIN app_role ar ON ur.role_id = ar.id WHERE ur.user_email = :user_email",nativeQuery = true)
 	List<String> getRoleAssigned(String user_email);
-	
+
 	@Query(value="select ar.role_name from user_temprory ur  JOIN app_role ar ON ur.role_id = ar.id WHERE ur.user_email = :user_email",nativeQuery = true)
 	List<String> getRoleTempBeforeApproved(String user_email);
-	
+
 	@Query(value="select * from user_master where user_email in (:userMails)",nativeQuery = true)
 	List<AdminUserMaster> userdata(String[] userMails);
-	
-	
-	
+
+
+
 	@Query(value = "SELECT \r\n"
 			+ "        um.user_email,\r\n"
 			+ "        ut.role_id,\r\n"
@@ -159,20 +159,22 @@ public interface AdminUserMasterRepository extends JpaRepository<AdminUserMaster
 			+ "\r\n"
 			+ "", nativeQuery = true)	
 	List<Object[]> findAllAdminUserPrivileges1();
-	
-	 boolean existsByUserEmail(String userEmail);
-	 
-	 @Query(value = "SELECT * FROM pgadmin.user_master ORDER BY create_at DESC", nativeQuery = true)
-	 List<AdminUserMaster> findAllAdminUsersOrderedByCreationDateDesc();
 
-	 @Query(value = "SELECT " +
-	            "(SELECT COUNT(*) FROM pgusers.user_master WHERE user_pin IS NOT NULL) AS activeTenants, " +
-	            "(SELECT COUNT(DISTINCT pg_owner_id) FROM pgcommon.pg_owner_property_status WHERE status = true) AS activeOwnersCount, " +
-	            "(SELECT COUNT(*) FROM pgcommon.pg_owner_property_status WHERE status = true) AS activePropertiesCount",
-	            nativeQuery = true)
+	boolean existsByUserEmail(String userEmail);
+
+	@Query(value = "SELECT * FROM pgadmin.user_master ORDER BY create_at DESC", nativeQuery = true)
+	List<AdminUserMaster> findAllAdminUsersOrderedByCreationDateDesc();
+
+	@Query(value = "SELECT " +
+			"(SELECT COUNT(*) FROM pgusers.user_master WHERE user_pin IS NOT NULL) AS activeTenants, " +
+			"(SELECT COUNT(DISTINCT pg_owner_id) FROM pgcommon.pg_owner_property_status WHERE status = true) AS activeOwnersCount, " +
+			"(SELECT COUNT(*) FROM pgcommon.pg_owner_property_status WHERE status = true) AS activePropertiesCount",
+			nativeQuery = true)
 	List<Object[]> getUsersWithNonNullPinAndActiveOwnersPropertiesCount();
-		
-				
 
-	
+	@Query(value="select user_profile_picture from pgadmin.user_master \r\n"
+			+ "	where user_email =:emailId",nativeQuery = true)
+	byte[] findProilePhoto(String emailId);
+
+
 }
