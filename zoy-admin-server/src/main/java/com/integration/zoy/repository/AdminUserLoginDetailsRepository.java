025bda69e51cfe1bc3f582c24ebd6d2ed8749e80
madhankumarram <madhan.a@mydbq.com>
2024-12-31 17:48:40 +0530

    password expire changes

--- zoy-admin-server/src/main/java/com/integration/zoy/repository/AdminUserLoginDetailsRepository.java
+++ zoy-admin-server/src/main/java/com/integration/zoy/repository/AdminUserLoginDetailsRepository.java
@@ -1,5 +1,7 @@
 package com.integration.zoy.repository;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -13,6 +15,7 @@ import com.integration.zoy.entity.AdminUserLoginDetails;
 
 @Repository
 public interface AdminUserLoginDetailsRepository extends JpaRepository<AdminUserLoginDetails, String> {
+	
 	@Query(value = "select * from pgadmin.user_login_details ul where lower(ul.user_email)=:email",nativeQuery = true)
 	Optional<AdminUserLoginDetails> findRegisterEmail(String email);
 	
@@ -42,5 +45,36 @@ public interface AdminUserLoginDetailsRepository extends JpaRepository<AdminUser
 	@Query(value = "UPDATE pgadmin.admin_users_lock SET attempt_sequence = 0 WHERE user_email = :email", nativeQuery = true)
 	void resetAttemptSequence(String email);
 	
+	@Transactional
+	@Modifying
+	@Query(value = "DELETE FROM admin_users_lock \n"
+			+ "where username in (select user_email from user_login_details \n"
+			+ "where (:curdt > (last_change_on + INTERVAL '45 days' )\n"
+			+ "and is_active = true and is_lock = false)) ",nativeQuery = true)
+	void deleteDataInAdminUsersLockDetails(Timestamp curdt);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE \n"
+			+ "user_login_details SET is_lock = true\n"
+			+ "WHERE (:curdt > (last_change_on + INTERVAL '45 days')  ) \n"
+			+ "	       AND is_active = true AND is_lock = false ",nativeQuery = true)
+	void doLockAfterPWExpired(Timestamp curdt);
+	
+	
+	@Query(value = "SELECT um.user_email, " +
+            "COALESCE(CONCAT(COALESCE(um.first_name, ''), ' ', COALESCE(um.last_name, '')), '') AS userName, " +
+            "(DATE(:curdt) - DATE(uld.last_change_on)) AS days " + // Calculate the difference in days directly
+            "FROM user_login_details uld " +
+            "JOIN user_master um ON um.user_email = uld.user_email " +
+            "WHERE :curdt > (uld.last_change_on + INTERVAL '38 days') " +
+            "AND :curdt <= (uld.last_change_on + INTERVAL '45 days') " +
+            "AND uld.last_change_on IS NOT NULL " +
+            "AND uld.is_active = true " +
+            "AND uld.is_lock = false", nativeQuery = true)
+	List<String[]> getPasswordWarnerMailsData(Timestamp curdt);
+
+
+
 	
 }
