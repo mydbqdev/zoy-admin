7a204418d99c26995becb481280be3abb1693b8e
Pavan4517 <pavan.d@mydbq.com>
2025-01-02 14:25:32 +0530

    tenant-details-api

--- zoy-admin-server/src/main/java/com/integration/zoy/controller/TenantMasterImpl.java
+++ zoy-admin-server/src/main/java/com/integration/zoy/controller/TenantMasterImpl.java
@@ -4,6 +4,7 @@ import org.simpleframework.xml.core.Validate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.integration.zoy.utils.PaginationRequest;
 
@@ -26,4 +27,16 @@ public interface TenantMasterImpl {
 	@GetMapping(value = "/zoy_admin/manage-tenants",
 	produces = { "application/json" })
 	ResponseEntity<String> zoyTenantManagement(@RequestBody PaginationRequest paginationRequest);
+	
+	
+	@Operation(summary = "Get Tenant Details", description = "Getting Tenant Management Details", security = {
+			@SecurityRequirement(name = "basicAuth")}, tags={ "Tenant Management" })
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json")),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "404", description = "Not Found"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error") })
+	@GetMapping(value = "/zoy_admin/manage-tenants-details",
+	produces = { "application/json" })
+	ResponseEntity<String> zoyTenantManagementDetails(@RequestParam("tenantid") String tenantid);
 }
