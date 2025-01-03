55390b87af87f27412cf2752d33a82097829ceb1
anand-dbq <anand.m@mydbq.com>
2025-01-03 17:05:26 +0530

    400 error message changes

--- zoyAdminAngular/src/app/components/signup/signup.component.ts
+++ zoyAdminAngular/src/app/components/signup/signup.component.ts
@@ -63,12 +63,12 @@ export class SignupComponent implements OnInit {
 			//  this.spinner.hide();
 			  let str;
 				if(error.status==400){
-				str=error.error;
+				str=error.error.error;
 				}else{
-				  str=error.message;
+				  str=error.error.message;
 				  str=str.substring(str.indexOf(":")+1);
 				}
-				console.log("Error:"+str);
+				console.log("Error:",str);
 				this.errorMsg=str;
 			}
 			if(error.status !== 401 ){this.notifyService.showError(this.errorMsg, "");}
--- zoyAdminAngular/src/app/profile/activity-log/activity-log.component.ts
+++ zoyAdminAngular/src/app/profile/activity-log/activity-log.component.ts
@@ -178,12 +178,12 @@ export class ActivityLogComponent implements OnInit, AfterViewInit {
 		  } else {
 			let str;
 			if (error.status == 400) {
-			  str = error.error;
+			  str = error.error.error;
 			} else {
-			  str = error.message;
+			  str = error.error.message;
 			  str = str.substring(str.indexOf(":") + 1);
 			}
-			console.log("Error:" + str);
+			console.log("Error:" ,str);
 			this.errorMsg = str;
 		  }
 		  if(error.status !== 401 ){this.notifyService.showError(this.errorMsg, "");}
@@ -247,9 +247,9 @@ export class ActivityLogComponent implements OnInit, AfterViewInit {
 				} else {
 				  let str;
 				  if (error.status == 400) {
-					str = error.error;
+					str = error.error.error;
 				  } else {
-					str = error.message;
+					str = error.error.message;
 					str = str.substring(str.indexOf(":") + 1);
 				  }
 				  console.log("Error:" + await str.text());
