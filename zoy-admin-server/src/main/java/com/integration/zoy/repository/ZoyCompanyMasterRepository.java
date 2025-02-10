537c3748c390e9e02a40b2272ec5e5b1890a4cc9
Pavan4517 <pavan.d@mydbq.com>
2025-02-10 15:28:40 +0530

    company master details

---
+++ zoy-admin-server/src/main/java/com/integration/zoy/repository/ZoyCompanyMasterRepository.java
@@ -0,0 +1,9 @@
+package com.integration.zoy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.integration.zoy.entity.ZoyCompanyMaster;
+
+public interface ZoyCompanyMasterRepository extends JpaRepository<ZoyCompanyMaster, String>{
+
+}
--- zoy-admin-server/src/main/java/com/integration/zoy/repository/ZoyCompanyProfileMasterRepository.java
+++ zoy-admin-server/src/main/java/com/integration/zoy/repository/ZoyCompanyProfileMasterRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.integration.zoy.entity.ZoyCompanyProfileMaster;
 @Repository
 public interface ZoyCompanyProfileMasterRepository extends JpaRepository<ZoyCompanyProfileMaster, String>{
-
+	
 }
