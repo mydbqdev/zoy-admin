79c1d42a4e7bf8a51eecaac16c90f238ff054548
BalajiDBQ <balaji.r@mydbq.com>
2025-02-27 15:28:08 +0530

    pg owner property status entity changes

--- zoy-admin-server/src/main/java/com/integration/zoy/entity/PgOwnerPropertyStatus.java
+++ zoy-admin-server/src/main/java/com/integration/zoy/entity/PgOwnerPropertyStatus.java
@@ -1,20 +1,57 @@
 package com.integration.zoy.entity;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 @Entity
 @Table(name = "pg_owner_property_status", schema = "pgcommon")
 public class PgOwnerPropertyStatus {
-
 
 	@EmbeddedId
 	private PgOwnerPropertyStatusId id;
 
 	@Column(name = "status", nullable = false)
 	private boolean status = false;
+
+	@Column(name = "status_type")
+	private String statusType;
+
+	@CreationTimestamp
+	@Column(name = "updated_timestamp")
+	private Timestamp updatedOn;
+
+	@Column(name = "status_reason")
+	private String statusReason;
+
+	public String getStatusType() {
+		return statusType;
+	}
+
+	public void setStatusType(String statusType) {
+		this.statusType = statusType;
+	}
+
+	public Timestamp getUpdatedOn() {
+		return updatedOn;
+	}
+
+	public void setUpdatedOn(Timestamp updatedOn) {
+		this.updatedOn = updatedOn;
+	}
+
+	public String getStatusReason() {
+		return statusReason;
+	}
+
+	public void setStatusReason(String statusReason) {
+		this.statusReason = statusReason;
+	}
 
 	public PgOwnerPropertyStatusId getId() {
 		return id;
@@ -31,8 +68,5 @@ public class PgOwnerPropertyStatus {
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
-
-
-
 
 }
