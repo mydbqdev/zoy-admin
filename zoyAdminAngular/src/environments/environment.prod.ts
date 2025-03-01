f1f811387dc8b01dec03a88b431781e477a4b40a
mahendradbq <125247980+mahendradbq@users.noreply.github.com>
2025-03-01 11:32:01 +0530

    Update environment.prod.ts

--- zoyAdminAngular/src/environments/environment.prod.ts
+++ zoyAdminAngular/src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-    //basePath:'http://45.129.87.152:8082/api-admin/',
-  basePath:'https://api-admin.zoypg.com/api-admin/',
-  websocketBasePath:'https://api-admin.zoypg.com/api-admin/',
+  basePath:'http://45.129.87.152:8082/api-admin/',
+  //basePath:'https://api-admin.zoypg.com/api-admin/',
+  websocketBasePath:'http://45.129.87.152:8082/api-admin/',
+  //websocketBasePath:'https://api-admin.zoypg.com/api-admin/',
   basePathExternal:'https://apis.zoypg.com/',
   oDataBlockSize:100,
   defMenuEnable:{
