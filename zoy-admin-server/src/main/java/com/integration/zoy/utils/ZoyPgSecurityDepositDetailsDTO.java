9e87f705f3123dbb8898e22e8a4f0d26e46cdac1
BalajiDBQ <balaji.r@mydbq.com>
2025-03-11 10:43:18 +0530

    security dto code

--- zoy-admin-server/src/main/java/com/integration/zoy/utils/ZoyPgSecurityDepositDetailsDTO.java
+++ zoy-admin-server/src/main/java/com/integration/zoy/utils/ZoyPgSecurityDepositDetailsDTO.java
@@ -1,6 +1,5 @@
 package com.integration.zoy.utils;
 
-
 import java.math.BigDecimal;
 
 import com.google.gson.annotations.SerializedName;
@@ -9,49 +8,83 @@ public class ZoyPgSecurityDepositDetailsDTO {
 
 	@SerializedName("depositId")
 	private String depositId;
-	
+
 	@SerializedName("minimumDeposit")
 	private BigDecimal minimumDeposit;
-	
+
 	@SerializedName("maximumDeposit")
 	private BigDecimal maximumDeposit;
-	
+
 	@SerializedName("effectiveDate")
 	private String effectiveDate;
-	
+
 	@SerializedName("isApproved")
 	private boolean isApproved;
-	
+
+	@SerializedName("approvedBy")
+	private String approvedBy;
+
+	@SerializedName("createdBy")
+	private String createdBy;
+
 	public String getDepositId() {
 		return depositId;
 	}
+
 	public void setDepositId(String depositId) {
 		this.depositId = depositId;
 	}
+
 	public BigDecimal getMinimumDeposit() {
 		return minimumDeposit;
 	}
+
 	public void setMinimumDeposit(BigDecimal minimumDeposit) {
 		this.minimumDeposit = minimumDeposit;
 	}
+
 	public BigDecimal getMaximumDeposit() {
 		return maximumDeposit;
 	}
+
 	public void setMaximumDeposit(BigDecimal maximumDeposit) {
 		this.maximumDeposit = maximumDeposit;
 	}
-	public String getEffectiveDate() {
-		return effectiveDate;
-	}
-	public void setEffectiveDate(String effectiveDate) {
-		this.effectiveDate = effectiveDate;
-	}
+
 	public boolean getIsApproved() {
 		return isApproved;
 	}
+
 	public void setIsApproved(boolean isApproved) {
 		this.isApproved = isApproved;
 	}
 
+	public String getEffectiveDate() {
+		return effectiveDate;
+	}
+
+	public void setEffectiveDate(String effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+
+	public void setApproved(boolean isApproved) {
+		this.isApproved = isApproved;
+	}
+
+	public String getApprovedBy() {
+		return approvedBy;
+	}
+
+	public void setApprovedBy(String approvedBy) {
+		this.approvedBy = approvedBy;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
 
 }
