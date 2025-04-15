a6b44d4062f41bdcc45b6ab016ad7be8e19eca6d
BalajiDBQ <balaji.r@mydbq.com>
2025-04-15 19:32:02 +0530

    special amminities and rental agreement

--- zoy-admin-server/src/main/java/com/integration/zoy/controller/ZoyAdminMasterImpl.java
+++ zoy-admin-server/src/main/java/com/integration/zoy/controller/ZoyAdminMasterImpl.java
@@ -595,6 +595,16 @@ public interface ZoyAdminMasterImpl {
 	produces = { "application/json" },
 	consumes = { "multipart/form-data"})
 	ResponseEntity<String> zoyAdminConfigUpdateRentalAgreementdocument(@RequestParam("rentalAgreementDocId")String rentalAgreementDocId,@RequestPart(value = "file",required = true) MultipartFile file);
+	
+	@Operation(summary = "Get Rental Agreement details",description = "Fetches all Rental Agreement documents",security = { @SecurityRequirement(name = "basicAuth") },tags = { "Admin Master" })
+		@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json")),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "404", description = "Not Found"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error")
+		})
+		@GetMapping(value = "/zoy_admin/rental-agreements", produces = { "application/json" })
+		public ResponseEntity<String> zoyAdminConfigGetRentalAgreementDocuments();
 }
 
 
