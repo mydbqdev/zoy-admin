67ec5b1d132b8e519b68c614b8dd85946118722d
mahen <mahendrabehera@mydbq.com>
2025-04-15 07:57:59 +0530

    changes

--- zoyAdminAngular/src/app/sales-person/sales/components/sales.component.ts
+++ zoyAdminAngular/src/app/sales-person/sales/components/sales.component.ts
@@ -86,20 +86,6 @@ export class SalesComponent implements OnInit, AfterViewInit {
 	}
 	selectedModel: string = 'generate';
 
-	// Method to update the selected button
-	selectButton(button: string): void {
-	  this.selectedModel = button;
-	  if(this.selectedModel =='generated'){
-		this.paramFilter.searchText=null;
-		this.param.filter=this.paramFilter; 
-		 this.getSalesPerson();
-		 this.submitted=false;
-		 this.form.reset();
-	  }else{
-		this.searchText='';
-	  }
-
-	}
 	ngOnDestroy() {
 		if (this.mySubscription) {
 			this.mySubscription.unsubscribe();
@@ -120,6 +106,7 @@ export class SalesComponent implements OnInit, AfterViewInit {
 			]],
 			empId:[''],
 		  });
+
 	}
 	ngAfterViewInit() {
 		this.sidemenuComp.expandMenu(9);
@@ -127,6 +114,25 @@ export class SalesComponent implements OnInit, AfterViewInit {
 		this.dataService.setHeaderName("Sales Person");
 	}
 
+	// Method to update the selected button
+	selectButton(button: string): void {
+		this.selectedModel = button;
+		if(this.selectedModel =='generated'){
+		this.param.pageIndex=0;//this.paginator.pageIndex;
+		this.param.pageSize=this.pageSize;
+		this.param.sortDirection="desc";
+		this.param.sortActive="created_date";
+		  this.paramFilter.searchText=null;
+		  this.param.filter=this.paramFilter; 
+		   this.getSalesPerson();
+		   this.columnSortDirections["created_date"] = "desc";
+		   this.submitted=false;
+		   this.form.reset();
+		}else{
+		  this.searchText='';
+		}
+  
+	  }
 	numberOnly(event): boolean {
 		const charCode = (event.which) ? event.which : event.keyCode;
 		if (charCode > 31 && (charCode < 48 || charCode > 57)) {
@@ -237,26 +243,12 @@ export class SalesComponent implements OnInit, AfterViewInit {
 
 		this.param.pageIndex=0
 		this.paginator.pageIndex=0;
+		this.param.pageSize= this.paginator.pageSize;
 		this.param.filter=this.paramFilter;
 		this.getSalesPerson();
 			}
 		}
-		getRetrieveData(){
-			this.dataSource.sort = this.sort;
-			this.dataSource.paginator = this.paginator;
-			this.param.pageIndex=this.paginator.pageIndex;
-			this.param.pageSize= this.paginator.pageSize;
-			this.param.sortDirection="desc";
-			this.param.sortActive="created_date";
-			this.paramFilter.searchText=null;
-			this.paramFilter.status=null;
-			this.param.filter=this.paramFilter;
-			setTimeout(()=>{
-			  this.getSalesPerson();
-			 },100);
-			 this.columnSortDirections["created_date"] = "desc";
-		  }
-			
+
 	getSalesPerson(){
 		this.authService.checkLoginUserVlidaate();
 		this.spinner.show();
