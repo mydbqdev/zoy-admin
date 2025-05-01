275e1df75710631e761b54f10d75dc14c1223c64
mahen <mahendrabehera@mydbq.com>
2025-05-01 18:56:18 +0530

    changes

--- zoy-admin-server/src/main/java/com/integration/zoy/controller/ZoyAdminSupportController.java
+++ zoy-admin-server/src/main/java/com/integration/zoy/controller/ZoyAdminSupportController.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
