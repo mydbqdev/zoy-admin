77db2895cb869afacb9c37c79c6e234b0b0c826c
pavangukanti <pavan.g@mydbq.com>
2025-05-18 13:32:34 +0530

    Update environment.prod.ts

--- zoyAdminAngular/src/environments/environment.prod.ts
+++ zoyAdminAngular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //basePath:'http://45.129.87.152:8082/api-admin/',
-  basePath:'https://api-admin-preprod.zoypg.com/api-admin/',
+  basePath:'http://45.129.87.152:8082/api-admin/',
+  //basePath:'https://api-admin-preprod.zoypg.com/api-admin/',
   //websocketBasePath:'https://api-admin.zoypg.com/api-admin/',
   websocketBasePath:'wss://api-admin-preprod.zoypg.com/api-admin/',
   basePathExternal:'https://api-preprod.zoypg.com:30001',
